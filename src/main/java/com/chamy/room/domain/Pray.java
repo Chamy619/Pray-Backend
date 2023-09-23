616f7a6419d2fb4e1aa14302dc2a2ab1fd1e1bfb
Chamy619 <yd95069506@naver.com>
2023-09-23 17:28:06 +0900

    feat: Pray Token 동일성 체크 메서드

--- src/main/java/com/chamy/room/domain/Pray.java
+++ src/main/java/com/chamy/room/domain/Pray.java
@@ -5,13 +5,13 @@ import com.chamy.common.util.TokenGenerator;
 import io.micrometer.common.util.StringUtils;
 import lombok.Getter;
 
+@Getter
 public class Pray {
 
     private static final String PREFIX = "pra_";
 
     private String token;
 
-    @Getter
     private String topic;
 
     public Pray(String topic) {
@@ -21,5 +21,9 @@ public class Pray {
 
         this.token = TokenGenerator.generate(PREFIX);
         this.topic = topic;
+    }
+
+    public boolean isSameToken(String token) {
+        return this.token.equals(token);
     }
 }
